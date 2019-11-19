36ca241632213f10fd79031c5499a49c9301527a
fengdongqing <fengdongqing@cmhi.chinamobile.com>
2019-11-19 12:39:28 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # k8s_init-pull_image
-a shell for pull images from mainland
+a shell for pull images from mainland  
+[refer to](https://blog.csdn.net/jinguangliu/article/details/82792617#commentsedit)  
+[refer to](https://blog.csdn.net/nklinsirui/article/details/80581286)
---
+++ pull_image.sh
@@ -0,0 +1,42 @@
+# @Author: dongqing feng
+# @Date:   2019-11-19 09:45:24
+# @Last Modified by:   Holly
+# @Last Modified time: 2019-11-19 11:29:00
+
+#!/bin/bash
+set -e
+
+# Check version in https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
+# Search "Running kubeadm without an internet connection"
+# For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
+
+#kube-apiserver:v1.14.6
+#kube-controller-manager:v1.14.6
+#kube-scheduler:v1.14.6
+#kube-proxy:v1.14.6
+#pause:3.1
+#coredns:1.3.1
+
+KUBE_VERSION=1.14.6
+KUBE_PAUSE_VERSION=3.1
+ETCD_VERSION=3.3.10
+DNS_VERSION=1.3.1
+
+GCR_URL=k8s.gcr.io
+ALIYUN_REGISTRY=registry.cn-hangzhou.aliyuncs.com/k8s_leaf
+
+images=(kube-apiserver:${KUBE_VERSION}
+	kube-controller-manager:${KUBE_VERSION}
+	kube-scheduler:${KUBE_VERSION}
+	kube-proxy:${KUBE_VERSION}
+	pause：${KUBE_PAUSE_VERSION}
+	etcd：${ETCD_VERSION}
+	coredns:${DNS_VERSION})
+
+for iamge_name in ${images[@]} ; do
+	docker pull $ALIYUN_REGISTRY/$iamge_name
+	docker tag $ALIYUN_REGISTRY/iamge_name $GCR_URL/$iamge_name
+	docker rmi $ALIYUN_REGISTRY/$image_name
+done
+
+docker images
